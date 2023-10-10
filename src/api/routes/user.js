51411da35b9da92b81a7d2b1211086740e66d4a6
mtellami <moadtell@gmail.com>
2023-10-10 04:15:40 +0100

    start login endpoint

--- src/api/routes/user.js
+++ src/api/routes/user.js
@@ -2,9 +2,11 @@ const express = require('express')
 
 // Controllers
 const Register = require('../controllers/user/Register')
+const Login = require('../controllers/user/Login')
 
 const router = express.Router()
 
 router.post('/user/register', Register)
+router.post('/user/login', Login)
 
 module.exports = router
